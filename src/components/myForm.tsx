3495bc40061081f0e52db33011ae3339ea6a9f79
sajjadabd <mynameissajjad@gmail.com>
2023-09-11 16:36:54 +0330

    style a little on the form appearance

--- src/components/myForm.tsx
+++ src/components/myForm.tsx
@@ -17,42 +17,47 @@ const MyForm: React.FC = () => {
   );
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
+    <form 
+    onSubmit={handleSubmit}
+    >
+      <div >
         <label htmlFor='name'>Name</label>
         <input
           type="text"
           name="name"
+          placeholder='Name'
           value={values.name}
           onChange={handleChange}
           onBlur={handleBlur}
           data-testid="name"
         />
-        {touched.name && errors.name && <div data-testid="name-error">{errors.name}</div>}
+        {touched.name && errors.name && <div className="error" data-testid="name-error">{errors.name}</div>}
       </div>
       <div>
         <label htmlFor='email'>Email</label>
         <input
           type="text"
           name="email"
+          placeholder='Email'
           value={values.email}
           onChange={handleChange}
           onBlur={handleBlur}
           data-testid="email"
         />
-        {touched.email && errors.email && <div data-testid="email-error">{errors.email}</div>}
+        {touched.email && errors.email && <div className="error" data-testid="email-error">{errors.email}</div>}
       </div>
       <div>
         <label htmlFor='password'>Password</label>
         <input
           type="password" // Use 'password' type for password input
           name="password"
+          placeholder='Password'
           value={values.password}
           onChange={handleChange}
           onBlur={handleBlur}
           data-testid="password"
         />
-        {touched.password && errors.password && <div>{errors.password}</div>}
+        {touched.password && errors.password && <div className="error" >{errors.password}</div>}
       </div>
       {/* Add more form fields */}
       <button 
